f8202913e58227048e5e84930cc9e42526e3d7ae
Kuncung <rickowijaya@gmail.com>
2023-02-04 14:25:48 +0700

    Test for agora

---
+++ AgoraTest/src/settings.js
@@ -0,0 +1,3 @@
+const appId = "e3baae8ab0514f6aa17a343cfc315e5e";
+const token =
+    "007eJxTYHiaOeVzXeZPtfojfzyvpJ8+N0/2w+YTi8+wbLn4L5tVN0NQgSHVOCkxMdUiMcnA1NAkzSwx0dA80djEODkt2djQNNU0tYL1XnJDICODtIk5IyMDBIL4LAy+iZl5DAwAUhchiw==";
