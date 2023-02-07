bb24b5b2c3108f6b463f5f2df48a428f95e1c055
fahrisshohib <fahrisshohib@gmail.com>
2023-02-07 20:28:10 +0700

    remove server-2

--- server-2/server/__test__/course.test.js
+++
@@ -1,6 +0,0 @@
-const app = require("../app");
-const request = require("supertest");
-const { sequelize } = require("../models");
-const { queryInterface } = sequelize;
-
-
--- server-2/server/helpers/bcrypt.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-const hashPassword = (password) => bcrypt.hashSync(password, 10);
-const comparePassword = (password, hash) => bcrypt.compareSync(password, hash);
-
-
-
-module.exports = { hashPassword, comparePassword }
