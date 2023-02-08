a2ef44ade5e81aed9fe85003c2e27b5b5ee8f3b6
Kuncung <rickowijaya@gmail.com>
2023-02-08 11:36:07 +0700

    Rework router and controller for instructor detail

--- server-2/server/routers/instructorRouter.js
+++ server-2/server/routers/instructorRouter.js
@@ -1,15 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const instructorController = require('../controllers/instructorController');
+const instructorController = require("../controllers/instructorController");
+
+const {
+    authenticationInstructor,
+    authenticationStudent,
+} = require("../middlewares/authentication");
 
 //* Multer
-const multer = require('multer');
-const { storage } = require('../cloudinary/index');
+const multer = require("multer");
+const { storage } = require("../cloudinary/index");
 const upload = multer({ storage });
 
-router.get('/', instructorController.getAllInstructors);
-router.post('/register', upload.single('image'), instructorController.register);
-router.post('/login', instructorController.login);
-router.get('/:id', instructorController.getOneInstructor);
+router.get("/", instructorController.getAllInstructors);
+router.post("/register", upload.single("image"), instructorController.register);
+router.post("/login", instructorController.login);
+router.get(
+    "/profile",
+    authenticationInstructor,
+    instructorController.getOneInstructor
+);
 
 module.exports = router;
